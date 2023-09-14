304bd4f667e446ee7e28f5e95c2eb3e03a862538
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-14 20:25:48 +0530

    java string methods part1

---
+++ stringMethod.js
@@ -0,0 +1,57 @@
+// JavaScript String Length
+
+// let text = "my name is raghuvaran";
+// let part = text.length;
+// console.log(part)
+
+
+// example.2
+// let raghu="The length property returns the length of a string";
+// let varan=raghu.length;
+// console.log(varan)
+
+// JavaScript String slice
+// example.1
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7,13);
+// console.log(part)
+
+// example.2 manam echina number tharvatha nundi manaki output anedhi show chestundhi 
+// NOTE: manam count 0 nundi cheyali eppudu
+
+// let line="slice() extracts a part of a string and returns the extracted part in a new string.";
+// let data= line.slice(8);
+// console.log(data)
+
+// let text="slice() extracts a part of a string and returns the extracted part in a new string.";
+// let data= text.slice(8,16);
+// console.log(data)
+
+// method 2
+// NOTE: last nundi count cheyadaniki -numbers tho use cheyali eka 
+
+// let text="slice() extracts a part of a string and returns the extracted part in a new string.";
+// let data= text.slice(-10,-1);
+// console.log(data)
+
+
+// 3.JavaScript String substring()....... negative number use cheyam and 0 nnundi count chestam and okka number manam echaka tharvata number anedhi contious kadhu malla 0 nundi start ithadi 
+// let java="The difference is that start and end values less than 0 are treated as 0 in substring().";
+// let course=java.substring(8)
+// console.log(course)
+
+// example.2
+
+// let part="apple, banana, kiwi";
+// let data=part.substring(7,9);
+// console.log(data)
+
+
+// 4.Replacing String Content
+
+// let text="The replace() method replaces a specified value with another value in a string";
+// console.log("before",text)
+// let newtext=text.replace("replace","add")
+// console.log("after",newtext)
+
