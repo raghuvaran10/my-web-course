fd6ba8be4c9ce3f351be6219b16eafeabca7188b
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-14 11:05:40 +0530

    java file basic

---
+++ head.js
@@ -0,0 +1,38 @@
+// let x=8
+// let y=10
+// let z=x+y
+// console.log(z)
+// string 
+
+// let name1="RAGHUVARAN"
+// let name2="MANTHENA"
+// let fullname=(name1+name2)
+// console.log(fullname)
+
+// let name1="raghuvaran"
+// let name2=" "
+// let name3="manthena"
+// let fullname=(name1+name2+name3)
+// console.log(fullname)
+
+// let name1="raghuvaran"
+// let name2=" "
+// let name3="manthena"
+// let fullname=(name1+name2+name3)
+// console.log(fullname,typeof(fullname))
+
+// let x=10
+// let name="raghu"
+// let total=x+name
+// console.log(total,typeof(total))
+
+// let raghu="manthena"
+// let varan='my best is a "my girl friend"'
+// let total=raghu+varan
+// console.log(total,typeof(total))
+
+let TEXT="before visit your microsoft"
+console.log(before,text)
+text.replace("Microsoft","W3Schools");
+let text="after visit your w3school"
+console.log(after,text)
