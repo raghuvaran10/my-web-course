9e1f9fe1dbf2629dd5b6340369a2cdc0305f98f8
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-16 20:34:15 +0530

    java operators files

---
+++ operator.js
@@ -0,0 +1,54 @@
+// let x=10
+// let y=5
+// let name=x+y
+// console.log(name)
+
+// let x=10
+// let y=5
+// let name=x-y
+// console.log(name)
+
+// let x=5
+// let y=20
+// let name=x-y
+// console.log(name)
+
+// let x=10
+// let y=5
+// let name=x*y
+// console.log(name)
+
+// let x=10
+// let y=5
+// let name=x/y
+// console.log(name)
+
+// let x=10
+// let y=2
+// let name=x**y
+// console.log(name)
+
+// let x=10
+// // let y=2
+// let name=++x
+// console.log(name)
+
+// let x=10
+// // let y=2
+// let name=--x
+// console.log(name)
+
+// let x=10
+// let y=2
+// let name=x%y
+// console.log(name)
+
+// let x=10
+// let y=(100+50)*x
+// // let name=x%y
+// console.log(y)
+
+let x=10
+let y=(100+50)*(10-5)/x
+// let name=x%y
+console.log(y)
