a21a64f93b25a456fbdcfc531f9d5afb3f29fc9a
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-16 11:37:58 +0530

    java array method

---
+++ array.js
@@ -0,0 +1,60 @@
+//     let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames[3]="raghuvaran"
+// console.log(allnames)
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames[1]="raghuvaran"
+// console.log(allnames)
+
+
+// push method   e method lo manam names r number add cheste adhi last ki add ithadhi contious ga 
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames.push="raghuvaran"
+// allnames.push="chinna"
+// console.log(allnames)
+
+// pop method   e method lo manam echina output lo nundi last ki a element ni remove cheyadaniki pop use chestam 
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames.pop()
+// allnames.pop()
+// console.log(allnames)
+
+// unshift method   e method lo manam starting lo elements add cheyadaniki unshift ni use chestam 
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames.unshift("saikumar")
+// console.log(allnames)
+
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames)
+// allnames.unshift("saikumar")
+// allnames.unshift("shiva")
+// console.log(allnames)
+
+// let allnames=[12,25,65,85,65,]
+// console.log(allnames)
+// allnames.unshift("99,35")
+// allnames.unshift("32,12")
+// console.log(allnames)
+
+// shift method     e method lo manam echina string or element lo starting lovi delete chestundhi shift anedhi 
+
+// let allnames=[12,25,65,85,65,]
+// console.log(allnames)
+// allnames.shift()
+// allnames.shift()
+// console.log(allnames)
+
+// e method lo manam echina string or element length anedhi teliyanapudu manam array use chesi length-1 use cheste manaki last lo vunna string vastundhi, last nundi 2nd kavali annapudu length-2 ala use cheyali 
+
+
+// let allnames=["raghu","varan","manthena"]
+// console.log(allnames[allnames.length-2])
