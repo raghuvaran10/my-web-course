5f577a339057d4ae1787c61b38f99337f64a945c
raghuvaran10 <raghu.mr199@gmail.com>
2023-09-18 13:24:39 +0530

    var,let,const differences

---
+++ java.js
@@ -0,0 +1,86 @@
+
+// variable method lo manam value ni reassign cheyavachu and redeclear cheyavachhu  
+
+// var x="raghu"
+// if (true) {
+// var x="varan"
+// console.log(x)
+// }
+// console.log(x)
+
+// var x=10
+// if (true) {
+// var x=19
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+// var x=10
+// if (true) {
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+// if (true) {
+// var x=19
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+// var x="raghu"
+// if (true) {
+// var x="manthena12"
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+// let method lo value ni reassign cheyvachu but redeclear cheyalemu 
+
+// let x="raghu"
+// if (true) {
+// let x="manthena12"
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+//  kindha example lo manam x ki value anedhi reassign chesam 
+
+// let x="raghu"
+// if (true) {
+// x="manthena12"
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+
+// let x="raghu"
+// if (true) {
+
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+
+
+// if (true) {
+// let x ="manthena12"
+// console.log("---block---", x)
+// }
+
+// const topic lo manam value ni reassign cheyalemu and redeclear cheyalemu 
+
+
+// const x="raghu"
+// if (true) {
+//  const x="manthena12"
+// console.log("---block---", x)
+// }
+// console.log("---global---",x)
+
+const x="raghu"
+if (true) {
+x="manthena12"
+console.log("---block---", x)
+}
+console.log("---global---",x)
+
